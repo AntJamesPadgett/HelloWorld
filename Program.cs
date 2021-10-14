477e0597143f00ba1e2dcaf57f763ba44d266a01
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-14 09:37:31 -0500

    Messing around with the console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Im Still Trying to Grasp Things!");
+        }
+    }
+}
